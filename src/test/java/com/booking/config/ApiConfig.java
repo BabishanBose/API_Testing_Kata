ebfa42d69c4f8c4f320196fcb1ed741116e2d988
babishan <babishan52@gmail.com>
2026-08-20 12:04:26 +0530

    feat: added booking API configuration

---
+++ src/test/java/com/booking/config/ApiConfig.java
@@ -0,0 +1,8 @@
+package com.booking.config;
+
+public final class ApiConfig {
+    private ApiConfig() {}
+
+    public static final String BASE_URL = "https://automationintesting.online/api";
+    public static final String BOOKING_ENDPOINT = "/booking";
+}
